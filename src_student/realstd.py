710954a2b43d4d6f9d75a60ed000b81c3769de67
Elizabeth Li <elizabethli@college.harvard.edu>
2023-09-27 11:06:22 -0400

    upload and request method changes from 09/26 OH

--- src_student/realstd.py
+++ src_student/realstd.py
@@ -18,6 +18,9 @@ class RealStd(Peer):
         print(("post_init(): %s here!" % self.id))
         self.dummy_state = dict()
         self.dummy_state["cake"] = "lie"
+        self.optimistic_unblock = ["", 0]
+        # can define additional global attributes here
+        # can track who you're currently optimistically unblocking, can also check round #, how many times you've uploaded to, etc.
     
     def requests(self, peers, history):
         """
@@ -50,33 +53,18 @@ class RealStd(Peer):
         
         # Sort peers by id.  This is probably not a useful sort, but other 
         # sorts might be useful
+        peers.sort(key=lambda p: p.id)
 
-        # peers.sort(key=lambda p: p.id)
-        #peers.sort(key=lambda p: p.AgentHistory)
-
-        # do we need to filter peers by interested peers, i.e., those that want something from ref client?
-        
-        # We're talking about one specific player p
-        # goal is to build up avg_download_from which will index by peer ids so that we can
-        # eventually sort by avg downloaded from rates (peers upload to RC)
-        # avg_download_from = [(0, 0) for i in range(len(peers))] # downloading from peers for the past two rounds
-        avg_download_from = {}
-        rounds = self.AgentHistory.downloads[-2:] # for the reference client, the list of downloads (and who we're downloading from) for the last two rounds
-        for round in rounds:
-            for download in round:
-                if download.from_id not in avg_download_from.keys():
-                    avg_download_from[download.from_id] = download.blocks
-                else:
-                    avg_download_from[download.from_id] += download.blocks
-
-        # order in decreasing order of av download rate received from peers, breaking ties at random
-        # and excluding any peers that have not sent any data -> we already don't have peers that haven't sent any data
-
-        preference = dict(sorted(avg_download_from.items(), key=lambda item: item[1], reverse = True))
-
-        # TO-DO: BREAK TIES BY RANDOM
-
-        
+        '''
+        when you're requesting, you request rarest first
+        you request for pieces -> someone only has access to a piece if they have all blocks
+        you send a request for everything you can, with the bandwidth you have available
+        '''
+        # rarity is based off of pieces, not blocks
+        # in peer, there's something about available pieces
+        # you go through all peers, see all avail ones, manually count through how many of each block is avail, and then 
+        # you can request multiple pieces from the same peer (request all the time as much as possible)
+        # you send requests in order of rarity within peers -> you request from everyone, and everyone can upload a different amount to you (but all in the same round)
 
         # request all available pieces from all peers!
         # (up to self.max_requests from each)
@@ -99,7 +87,7 @@ class RealStd(Peer):
 
     def uploads(self, requests, peers, history):
         """
-        requests -- a list of the requests for this peer for this round
+        requests -- a list of the requests for this peer for this round -> list of requester objects, within each can .requester_id
         peers -- available info about all the peers
         history -- history for all previous rounds
 
@@ -125,13 +113,70 @@ class RealStd(Peer):
             # change my internal state for no reason
             self.dummy_state["cake"] = "pie"
 
-            request = random.choice(requests)
+            request = random.choice(requests) # requests is the list of people who requested from you
             chosen = [request.requester_id]
             # Evenly "split" my upload bandwidth among the one chosen requester
             bws = even_split(self.up_bw, len(chosen))
 
+            '''to be deleted:'''
+            # We're talking about one specific player p
+            # goal is to build up avg_download_from which will index by peer ids so that we can
+            # eventually sort by avg downloaded from rates (peers upload to RC)
+            # avg_download_from = [(0, 0) for i in range(len(peers))] # downloading from peers for the past two rounds
+            '''End of bad code'''
+
+            requesters = [request.requester_id for request in requests]
+            avg_download_from = {}
+            rounds = history.downloads[-2:]
+            random.shuffle(rounds) # randomize order of lists; we randomize and then sort to break ties randomly!
+
+            for round in rounds:
+                for download in round:
+                    if download.from_id not in avg_download_from.keys():
+                        avg_download_from[download.from_id] = download.blocks
+                    else:
+                        avg_download_from[download.from_id] += download.blocks
+
+            preference = (sorted(avg_download_from.items(), key=lambda item: item[1], reverse = True))[:3]
+            # [(peer_id, numblocks), (peer_id, numblocks), (peer_id, numblocks)]
+            
         # create actual uploads out of the list of peer ids and bandwidths
         uploads = [Upload(self.id, peer_id, bw)
                    for (peer_id, bw) in zip(chosen, bws)]
             
         return uploads
+
+        '''OLD CODE'''
+        # avg_download_from = {}
+        # rounds = self.AgentHistory.downloads[-2:] # for the reference client, the list of downloads (and who we're downloading from) for the last two rounds
+        # for round in rounds:
+        #     for download in round:
+        #         if download.from_id not in avg_download_from.keys():
+        #             avg_download_from[download.from_id] = download.blocks
+        #         else:
+        #             avg_download_from[download.from_id] += download.blocks
+        '''END OF OLD CODE'''
+        
+
+        # for reference client, distribute bandwidth evenly
+
+        # everyone who sent request for piece is interested peers [requests]
+        # optimistic unblock: if get, then stay for 3 rounds max (or until they don't need it anymore)
+        # give them the option to have that spot for the next three rounds -> discrepancies
+        # have global var for the ID of the unblocked person -> 
+
+        # do we need to filter peers by interested peers, i.e., those that want something from ref client?
+        
+        
+
+        # order in decreasing order of av download rate received from peers, breaking ties at random
+        # and excluding any peers that have not sent any data -> we already don't have peers that haven't sent any data
+
+        # you take the maximum 3 and you put them in your uploads method (this is all in uploads)
+
+        # you prefer to upload to people who upload to you
+        # you have 3 normal spots in your reference client to upload to people, and there's one optimistic
+
+        # to unblock someone, you put their ID and their bandwidth in the uploads list
+
+        
